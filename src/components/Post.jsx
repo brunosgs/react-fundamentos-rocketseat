40fdc0a451fc633dc1480cd1cba7d1cbb14de70e
brunosgs <brunosoges@gmail.com>
2025-05-24 23:45:23 -0300

    feat: validando formulário para não enviar comentários com campos vazios.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,7 +29,13 @@ export function Post({ author, content, publishedAt }) {
    }
 
    function handleNewCommentChange() {
+      event.target.setCustomValidity('')
+
       setNewCommentText(event.target.value)
+   }
+
+   function handleNewCommentInvalid() {
+      event.target.setCustomValidity('Esse campo é obrigatório!')
    }
 
    function deleteComment(commentToDelete) {
@@ -39,6 +45,8 @@ export function Post({ author, content, publishedAt }) {
 
       setComments(commentsWithoutDeletedOne)
    }
+
+   const isNewCommentEmpty = newCommentText.trim().length === 0
 
    return (
       <article className={styles.post}>
@@ -75,9 +83,13 @@ export function Post({ author, content, publishedAt }) {
                placeholder='Deixe um comentário'
                value={newCommentText}
                onChange={handleNewCommentChange}
+               onInvalid={handleNewCommentInvalid}
+               required
             />
             <footer>
-               <button type="submit">Publicar</button>
+               <button type="submit" disabled={isNewCommentEmpty}>
+                  Publicar
+               </button>
             </footer>
          </form>
          <div className={styles.commentList}>
